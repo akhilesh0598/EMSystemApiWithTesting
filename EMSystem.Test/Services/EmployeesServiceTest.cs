46a41a81e04519074b423176e7cd65d7f8d9b4ce
akhilesh0598 <akhilesh.chaurasiya0598@gmail.com>
2022-05-26 11:24:53 +0530

    added and checking exception message

--- EMSystem.Test/Services/EmployeesServiceTest.cs
+++ EMSystem.Test/Services/EmployeesServiceTest.cs
@@ -19,32 +19,26 @@ namespace EMSystem.Test.Services
         {
             EmployeesRepositoryMock.MockResources();
             DepartmentsRepositoryMock.MockResources();
-
         }
         //Add method testing
         
         [Fact]
         public void AddWhenEmailIdAlreadyExist()
         {
-            
             var employeesService = new EmployeesService(EmployeesRepositoryMock.employeeRepositoryMock.Object, DepartmentsRepositoryMock.departmentsRepositoryMock.Object);
-            
-            Assert.Throws<IdFoundException>(() => employeesService.Add(EmployeeRequestData.GetEmployeeRequestWhenEmailIdExist()));
-
+            var ex = Assert.Throws<IdFoundException>(() => employeesService.Add(EmployeeRequestData.GetEmployeeRequestWhenEmailIdExist()));
+            Assert.Equal("In Employee Entity EmailId = 'akhil@test.com' already exist", ex.Message);
         }
         [Fact]
         public void AddWhenEmailIdNotExistAndDepartmentIdNotExist()
         {
-            
             var employeesService = new EmployeesService(EmployeesRepositoryMock.employeeRepositoryMock.Object, DepartmentsRepositoryMock.departmentsRepositoryMock.Object);
-
-            Assert.Throws<WrongDataInBodyException>(() => employeesService.Add(EmployeeRequestData.GetEmployeeRequestWhenEmailIdNotExistAndDepartmentIdNotExist()));
-
+            var ex=Assert.Throws<WrongDataInBodyException>(() => employeesService.Add(EmployeeRequestData.GetEmployeeRequestWhenEmailIdNotExistAndDepartmentIdNotExist()));
+            Assert.Equal("In Department Entity Id = '3' not found", ex.Message);
         }
         [Fact]
         public void AddWhenAllIsGood()
         {
-            
             var employeesService = new EmployeesService(EmployeesRepositoryMock.employeeRepositoryMock.Object, DepartmentsRepositoryMock.departmentsRepositoryMock.Object);
             employeesService.Add(EmployeeRequestData.GetEmployeeRequestWhenAllIsGood());
             EmployeesRepositoryMock.employeeRepositoryMock.Verify(x => x.Create(It.IsAny<EmployeeDTO>()), Times.Exactly(1));
@@ -55,38 +49,32 @@ namespace EMSystem.Test.Services
         {
             
             var employeesService = new EmployeesService(EmployeesRepositoryMock.employeeRepositoryMock.Object, DepartmentsRepositoryMock.departmentsRepositoryMock.Object);
-            Assert.Throws<IdNotFoundException>(() => employeesService.GetById("aman@test.com"));
-
+            var ex=Assert.Throws<IdNotFoundException>(() => employeesService.GetById("aman@test.com"));
+            Assert.Equal("In Employee Entity Id = 'aman@test.com' not found", ex.Message);
         }
         [Fact]
         public void GetByIdWhenIdFound()
         {
-            
             var employeesService = new EmployeesService(EmployeesRepositoryMock.employeeRepositoryMock.Object, DepartmentsRepositoryMock.departmentsRepositoryMock.Object);
-
             var result=employeesService.GetById("akhil@test.com");
             var res1 = JsonConvert.SerializeObject(result);
             var res2 = JsonConvert.SerializeObject(EmployeeResponseData.GetEmployeeResponse());
             Assert.Equal(res2,res1);
-            
         }
         //CountByDepartmentName method
         [Fact]
         public void CountByDepartmentName_WhenDepartmentNameNotExist()
         {
-            
             var employeesService = new EmployeesService(EmployeesRepositoryMock.employeeRepositoryMock.Object, DepartmentsRepositoryMock.departmentsRepositoryMock.Object);
-            Assert.Throws<IdNotFoundException>(() => employeesService.CountByDepartmentName("Sales"));
+            var ex=Assert.Throws<IdNotFoundException>(() => employeesService.CountByDepartmentName("Sales"));
+            Assert.Equal("In Department Entity Name = 'Sales' not found", ex.Message);
         }
         [Fact]
         public void CountByDepartmentName_WhenDepartmentNameExist()
         {
-            
             var employeesService = new EmployeesService(EmployeesRepositoryMock.employeeRepositoryMock.Object, DepartmentsRepositoryMock.departmentsRepositoryMock.Object);
-
             var result=employeesService.CountByDepartmentName("IT");
             Assert.Equal(2, result);
-
         }
     }
 }
